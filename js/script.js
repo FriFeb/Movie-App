3b022833d7289c9b85615ee1dae4d7d2a9619959
Fedir Denysenko <feddenisenko1@gmail.com>
2023-07-17 22:32:09 +0300

    Completed pagination

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const global = {
         type: '',
         page: 1,
         totalPages: 1,
+        totalResults: 0,
     },
     api: {
         apiKey: '59dd12cf642c9029deecd00d950fd3a6',
@@ -230,7 +231,11 @@ async function search() {
     global.search.term = urlParams.get('search-term');
 
     if (global.search.term !== '' && global.search.term !== null) {
-        const { results, total_pages, page } = await searchAPIData();
+        const { results, total_pages, page, total_results } = await searchAPIData();
+
+        global.search.page = page;
+        global.search.totalPages = total_pages;
+        global.search.totalResults = total_results;
 
         if (!results.length) {
             showAlert('No results found');
@@ -240,13 +245,18 @@ async function search() {
         showAlert('Results are found', 'success');
         displaySearchResults(results);
 
-        document.querySelector('#search-item').value = '';
     } else {
         showAlert('Please enter a search item');
     }
 }
 
 function displaySearchResults(results) {
+
+    // Clear previous resuts
+    document.querySelector('#search-results').innerHTML = '';
+    document.querySelector('#search-results-heading').innerHTML = '';
+    document.querySelector('#pagination').innerHTML = '';
+
     results.forEach(result => {
         const div = document.createElement('div');
 
@@ -268,14 +278,20 @@ function displaySearchResults(results) {
                 : result.name}</h5>
                 <p class="card-text">
                     <small class="text-muted">Release: ${global.search.type === 'movie'
-                    ? result.release_date
-                    : result.first_air_date}</small>
+                ? result.release_date
+                : result.first_air_date}</small>
                 </p>
             </div>
         `;
 
+        document.querySelector('#search-results-heading').innerHTML =
+            `<h2>${results.length} of ${global.search.totalResults}
+            for ${global.search.term}</h2>
+            `
         document.querySelector('#search-results').appendChild(div);
     });
+
+    displayPagination();
 }
 
 //  Show error alert 
@@ -287,6 +303,45 @@ function showAlert(message, className = 'error') {
     document.querySelector('#alert').appendChild(alertEl);
 
     setTimeout(() => alertEl.remove(), 3000);
+}
+
+//  Display Pagination
+function displayPagination() {
+    const div = document.createElement('div');
+
+    div.classList.add('pagination');
+
+    div.innerHTML = `
+    <button class="btn btn-primary" id="prev">Prev</button>
+    <button class="btn btn-primary" id="next">Next</button>
+    <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+    `
+
+    document.querySelector('#pagination').appendChild(div);
+
+    // Disable 'Prev' button on 1st page
+    if (global.search.page === 1) {
+        document.querySelector('#prev').disabled = true;
+    }
+
+    // Disable 'Next' button on last page
+    if (global.search.page === global.search.totalPages) {
+        document.querySelector('#next').disabled = true;
+    }
+
+    // Next page 
+    document.querySelector('#next').addEventListener('click', async () => {
+        global.search.page++;
+        const { results } = await searchAPIData();
+        displaySearchResults(results);
+    });
+
+    // Prev page 
+    document.querySelector('#prev').addEventListener('click', async () => {
+        global.search.page--;
+        const { results } = await searchAPIData();
+        displaySearchResults(results);
+    });
 }
 
 //  Display slider
@@ -376,7 +431,7 @@ async function searchAPIData(endpoint) {
 
     showSpinner();
 
-    const responce = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&query=${global.search.term}`, options);
+    const responce = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&query=${global.search.term}&page=${global.search.page}`, options);
     const data = await responce.json();
 
     hideSpinner();
